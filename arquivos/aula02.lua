e85cdfac0447bffde352d8aedbbb8bfc6c1d8d14
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-02-01 17:35:29 -0300

    Variáveis em Lua - Curso de Lua - Aula 02

---
+++ arquivos/aula02.lua
@@ -0,0 +1,16 @@
+--[[
+    tipos primitivos:
+]]
+
+nome = "Marcos"
+numero = 10
+valor = 5.2
+logica = true
+vazio = nil -- nil é como o null
+
+print("Tipo é: ", type(nome))   -- type mostrando o tipo da variael, a virgula é a concatenacao e não o +
+print(type(numero))
+print(type(valor))
+print(type(logica))
+print(type(vazio))
+print(type(type))
