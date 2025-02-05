8b5640ca3e7f74a4c81e73d7bea3f61e5d009cf6
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-02-05 15:52:10 -0300

    Comandos break e return em Lua - Curso de Lua - Aula 11

---
+++ arquivos/aula11.lua
@@ -0,0 +1,15 @@
+--[[
+    Break 
+    ele quebra a sequencia e sai de um bloco de codigo
+]]
+
+for i=0, 10 do
+
+    if i > 5 then
+        break
+    end
+
+    print(i)
+end
+
+print("saiu do laço")
